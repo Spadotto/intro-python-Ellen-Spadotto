88142291ad054de3a4cc2c0f6a046a3b8ceb786d
Ellen <52865288+Spadotto@users.noreply.github.com>
2021-05-21 19:03:03 -0300

    Desafios da 105 resolvidos

---
+++ desafio-105.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+'''
+    DESAFIO!!!
+    1) Crie uma lista com os 1000 primeiros numeros pares. Não use loop!
+    2) Crie uma lista com os numeros de 0 ate 99999999999999999999999999. Depois crie um loop para percorrer a lista ateh encontrar o primeiro multiplo de 5.
+    OBS.: Use sua linguagem de programacao favorita. Nao use funcoes/metodos prontos.
+'''
+
+print('\nDesafio 1:')
+listapares = [i for i in range(1000) if i % 2 == 0]
+print(listapares)
+
+print('\nDesafio 2:')
+lista = [i for i in range(99999999999999999999999999)]
+
+for i in range(len(lista)):
+    if i % 5 == 0:
+        print(i)
+        break
